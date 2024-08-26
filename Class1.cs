cc14feb57986c38980a5e7040d8cc11930bd9eb0
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2024-08-26 14:13:47 +0200

    Minimal repro

--- Class1.cs
+++ Class1.cs
@@ -3,17 +3,25 @@
 public interface IThird<out TSecond> where TSecond : IThird<TSecond>
 {
     internal static abstract int Metadata { get; }
+    internal abstract int MetadataInstance { get; }
 }
 
 public interface ISecond<out TSecond> : IThird<TSecond> where TSecond : ISecond<TSecond>
 {
     protected static abstract TSecond Create(int _);
+    protected abstract TSecond CreateInstance(int _);
 }
 
 public interface IFirst<TFirst> : ISecond<TFirst> where TFirst : IFirst<TFirst>
 {
     protected new static abstract int Metadata { get; }
+    protected new abstract int MetadataInstance { get; }
     protected static abstract TFirst Create(string initializer);
     protected new static abstract TFirst Create(int initializer);
     protected static abstract TFirst Create(decimal initializer);
+
+    protected abstract TFirst CreateInstance(string initializer);
+    protected new abstract TFirst CreateInstance(int initializer);
+    protected abstract TFirst CreateInstance(decimal initializer);
+
 }
--- Library.fs
+++ Library.fs
@@ -8,3 +8,8 @@ type First =
         static member Create(_: string) = Unchecked.defaultof<_>
         static member Create(_: int) = Unchecked.defaultof<_>
         static member Create(_: decimal) = Unchecked.defaultof<_>
+
+        member _.MetadataInstance = 42
+        member _.CreateInstance(_: string) = Unchecked.defaultof<_>
+        member _.CreateInstance(_: int) = Unchecked.defaultof<_>
+        member _.CreateInstance(_: decimal) = Unchecked.defaultof<_>
