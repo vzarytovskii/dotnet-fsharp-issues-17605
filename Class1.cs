b5a9b28aba63bd2503eeae011cc9111c556d19da
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2024-08-26 14:10:15 +0200

    Minimal repro

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace _17605;
+
+public interface IThird<out TSecond> where TSecond : IThird<TSecond>
+{
+    internal static abstract int Metadata { get; }
+}
+
+public interface ISecond<out TSecond> : IThird<TSecond> where TSecond : ISecond<TSecond>
+{
+    protected static abstract TSecond Create(int _);
+}
+
+public interface IFirst<TFirst> : ISecond<TFirst> where TFirst : IFirst<TFirst>
+{
+    protected new static abstract int Metadata { get; }
+    protected static abstract TFirst Create(string initializer);
+    protected new static abstract TFirst Create(int initializer);
+    protected static abstract TFirst Create(decimal initializer);
+}
---
+++ Library.fs
@@ -0,0 +1,10 @@
+﻿namespace _17605fs
+
+open _17605
+
+type First =
+    interface IFirst<First> with
+        static member Metadata = 42
+        static member Create(_: string) = Unchecked.defaultof<_>
+        static member Create(_: int) = Unchecked.defaultof<_>
+        static member Create(_: decimal) = Unchecked.defaultof<_>
